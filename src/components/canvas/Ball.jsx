17e819bc2f48c274779aeaa0a61d5b3372909ddd
AdenBinFarrukh <aden.binfarrukh.sirius@gmail.com>
2023-05-16 23:27:37 -0700

    Adding Portfolio website

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -56,63 +56,13 @@
 
 // export default BallCanvas;
 
-import React, { Suspense, useEffect, useState } from "react";
+import React from "react";
 import { Canvas } from "@react-three/fiber";
-import {
-    Decal,
-    Float,
-    OrbitControls,
-    Preload,
-    useTexture,
-} from "@react-three/drei";
+import { OrbitControls, Preload } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-const Ball = ({ imgUrl }) => {
-    const [decal] = useTexture([imgUrl]);
-
-    return (
-        <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-            <ambientLight intensity={0.25} />
-            <directionalLight position={[0, 0, 0.05]} />
-            <mesh castShadow receiveShadow scale={2.75}>
-                <icosahedronGeometry args={[1, 1]} />
-                <meshStandardMaterial
-                    color="#fff8eb"
-                    polygonOffset
-                    polygonOffsetFactor={-5}
-                    flatShading
-                />
-                <Decal
-                    position={[0, 0, 1]}
-                    rotation={[2 * Math.PI, 0, 6.25]}
-                    scale={1}
-                    map={decal}
-                    flatShading
-                />
-            </mesh>
-        </Float>
-    );
-};
-
 const BallCanvas = ({ icon }) => {
-    const [isMobile, setIsMobile] = useState(false);
-
-    useEffect(() => {
-        const mediaQuery = window.matchMedia("(max-width: 1000px)");
-        setIsMobile(mediaQuery.matches);
-
-        const handleMediaQueryChange = (event) => {
-            setIsMobile(event.matches);
-        };
-
-        mediaQuery.addEventListener("change", handleMediaQueryChange);
-
-        return () => {
-            mediaQuery.removeEventListener("change", handleMediaQueryChange);
-        };
-    }, []);
-
     return (
         <Canvas
             frameloop="demand"
@@ -120,7 +70,7 @@ const BallCanvas = ({ icon }) => {
             gl={{ preserveDrawingBuffer: true }}>
             <Suspense fallback={<CanvasLoader />}>
                 <OrbitControls enableZoom={false} />
-                {isMobile ? null : <Ball imgUrl={icon} />}
+                <image position={[0, 0, 0]} url={icon} />
             </Suspense>
 
             <Preload all />
